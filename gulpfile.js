1d8c7f304a85075cf910096e8231d258c80530b5
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2020-04-29 18:49:48 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+    'node_modules/jquery-timepicker/jquery.timepicker.js',
     'node_modules/slick-carousel/slick/slick.min.js',
     'node_modules/retinajs/dist/retina.min.js',
     'app/js/app.js', // app.js. Always at the end
